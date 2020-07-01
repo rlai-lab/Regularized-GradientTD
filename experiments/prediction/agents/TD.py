48f93bf55c8c8c400f81f971fcdb89c73d0da5c6
andy <andnpatterson@gmail.com>
2020-07-01 11:49:20 -0600

    add structure for experiments portion

---
+++ experiments/prediction/agents/TD.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class TD:
+    def __init__(self, features, params):
+        self.features = features
+        self.params = params
+
+        self.gamma = params['gamma']
+        self.alpha = params['alpha']
+
+        self.w = np.zeros(features)
+
+    def update(self, x, a, r, xp, rho):
+        v = self.w.dot(x)
+        vp = self.w.dot(xp)
+
+        delta = r + self.gamma * vp - v
+
+        self.w = self.w + self.alpha * rho * delta * x
+
+    def value(self, x):
+        # handle the case where x is a state*feature matrix
+        return np.dot(x, self.w)
