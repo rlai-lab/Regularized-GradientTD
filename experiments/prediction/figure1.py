13829a9b1d451dfa8e9d815aae22bc0d18d8353b
andy <andnpatterson@gmail.com>
2020-07-04 11:19:12 -0600

    add remainder of algorithms and problem sets

--- experiments/prediction/figure1.py
+++ experiments/prediction/figure1.py
@@ -3,19 +3,27 @@ import numpy as np
 # source found at: https://github.com/andnp/RlGlue
 from RlGlue import RlGlue
 from utils.Collector import Collector
-from utils.policies import actionArrayToPolicy
+from utils.policies import actionArrayToPolicy, matrixToPolicy
 from utils.rl_glue import RlGlueCompatWrapper
 from utils.errors import buildRMSPBE
 
 from environments.RandomWalk import RandomWalk, TabularRep, DependentRep, InvertedRep
+from environments.Boyan import Boyan, BoyanRep
+from environments.Baird import Baird, BairdRep
+
 from agents.TD import TD
+from agents.TDC import TDC
+from agents.HTD import HTD
+from agents.GTD2 import GTD2
+from agents.TDRC import TDRC
+from agents.Vtrace import Vtrace
 
 # --------------------------------
 # Set up parameters for experiment
 # --------------------------------
 
-RUNS = 10
-LEARNERS = [TD]
+RUNS = 5
+LEARNERS = [GTD2, TDC, Vtrace, HTD, TD, TDRC]
 
 PROBLEMS = [
     # 5-state random walk environment with tabular features
@@ -30,7 +38,12 @@ PROBLEMS = [
         'steps': 3000,
         # hardcode stepsizes found from parameter study
         'stepsizes': {
-            'TD': 0.01,
+            'TD': 0.03125,
+            'TDRC': 0.03125,
+            'TDC': 0.0625,
+            'GTD2': 0.03125,
+            'HTD': 0.03125,
+            'Vtrace': 0.03125,
         }
     },
     # 5-state random walk environment with dependent features
@@ -45,7 +58,12 @@ PROBLEMS = [
         'steps': 3000,
         # hardcode stepsizes found from parameter study
         'stepsizes': {
-            'TD': 0.01,
+            'TD': 0.03125,
+            'TDRC': 0.03125,
+            'TDC': 0.0625,
+            'GTD2': 0.0625,
+            'HTD': 0.03125,
+            'Vtrace': 0.03125,
         }
     },
     # 5-state random walk environment with inverted features
@@ -60,13 +78,64 @@ PROBLEMS = [
         'steps': 3000,
         # hardcode stepsizes found from parameter study
         'stepsizes': {
-            'TD': 0.01,
+            'TD': 0.125,
+            'TDRC': 0.125,
+            'TDC': 0.125,
+            'GTD2': 0.125,
+            'HTD': 0.125,
+            'Vtrace': 0.125,
+        }
+    },
+    # Boyan's chain
+    {
+        'env': Boyan,
+        'representation': BoyanRep,
+        # go LEFT 40% of the time
+        'target': matrixToPolicy([[.5, .5]] * 10 + [[1., 0.]] * 2),
+        # take each action equally
+        'behavior': matrixToPolicy([[.5, .5]] * 10 + [[1., 0.]] * 2),
+        'gamma': 1.0,
+        'steps': 10000,
+        # hardcode stepsizes found from parameter study
+        'stepsizes': {
+            'TD': 0.0625,
+            'TDRC': 0.0625,
+            'TDC': 0.5,
+            'GTD2': 0.5,
+            'HTD': 0.0625,
+            'Vtrace': 0.0625,
+        }
+    },
+    # Baird's Counter-example domain
+    {
+        'env': Baird,
+        'representation': BairdRep,
+        # go LEFT 40% of the time
+        'target': actionArrayToPolicy([6/7, 1/7]),
+        # take each action equally
+        'behavior': actionArrayToPolicy([0., 1.]),
+        'starting_condition': np.array([1, 1, 1, 1, 1, 1, 1, 10]),
+        'gamma': 0.99,
+        'steps': 20000,
+        # hardcode stepsizes found from parameter study
+        'stepsizes': {
+            'TD': 0.00390625,
+            'TDRC': 0.015625,
+            'TDC': 0.00390625,
+            'GTD2': 0.00390625,
+            'HTD': 0.00390625,
+            'Vtrace': 0.00390625,
         }
     },
 ]
 
 COLORS = {
     'TD': 'blue',
+    'TDC': 'green',
+    'TDRC': 'orange',
+    'GTD2': 'grey',
+    'Vtrace': 'red',
+    'HTD': 'purple',
 }
 
 # -----------------------------------
@@ -94,6 +163,8 @@ for run in range(RUNS):
             Rep = problem['representation']
             rep = Rep()
 
+            print(run, Env.__name__, Rep.__name__, Learner.__name__)
+
             # build the X, P, R, and D matrices for computing RMSPBE
             X, P, R, D = env.getXPRD(target, rep)
             RMSPBE = buildRMSPBE(X, P, R, D, problem['gamma'])
@@ -101,12 +172,17 @@ for run in range(RUNS):
             # build a new instance of the learning algorithm
             learner = Learner(rep.features(), {
                 'gamma': problem['gamma'],
-                'alpha': problem['stepsizes'][Learner.__name__]
+                'alpha': problem['stepsizes'][Learner.__name__],
+                'beta': 1,
             })
 
             # build an "agent" which selects actions according to the behavior
             # and tries to estimate according to the target policy
             agent = RlGlueCompatWrapper(learner, behavior, target, rep.encode)
+
+            # for Baird's counter-example, set the initial value function manually
+            if problem.get('starting_condition') is not None:
+                learner.w = problem['starting_condition'].copy()
 
             # build the experiment runner
             # ties together the agent and environment
@@ -125,13 +201,15 @@ for run in range(RUNS):
                     glue.start()
 
                 # evaluate the RMPSBE
-                w = learner.getWeights()
-                rmspbe = RMSPBE(w)
+                # subsample to reduce computational cost
+                if step % 100 == 0:
+                    w = learner.getWeights()
+                    rmspbe = RMSPBE(w)
 
-                #  create a unique key to store the data for this env/representation/agent tuple
-                data_key = f'{Env.__name__}-{Rep.__name__}-{Learner.__name__}'
-                # store the data in the "collector" until we need it for plotting
-                collector.collect(data_key, rmspbe)
+                    #  create a unique key to store the data for this env/representation/agent tuple
+                    data_key = f'{Env.__name__}-{Rep.__name__}-{Learner.__name__}'
+                    # store the data in the "collector" until we need it for plotting
+                    collector.collect(data_key, rmspbe)
 
             # tell the data collector we're done collecting data for this env/learner/rep combination
             collector.reset()
@@ -143,6 +221,17 @@ import matplotlib.pyplot as plt
 
 ax = plt.gca()
 f = plt.gcf()
+
+# get TDRC's baseline performance for each problem
+baselines = [None] * len(PROBLEMS)
+for i, problem in enumerate(PROBLEMS):
+    env = problem['env'].__name__
+    rep = problem['representation'].__name__
+
+    mean_curve, _, _ = collector.getStats(f'{env}-{rep}-TDRC')
+
+    # compute TDRC's AUC
+    baselines[i] = mean_curve.mean()
 
 # how far from the left side of the plot to put the bar
 offset = -3
@@ -161,6 +250,9 @@ for i, problem in enumerate(PROBLEMS):
         auc = mean_curve.mean()
         auc_stderr = stderr_curve.mean()
 
-        ax.bar(x, auc, yerr=auc_stderr, color=COLORS[learner])
+        relative_auc = auc / baselines[i]
+        relative_stderr = auc_stderr / baselines[i]
+
+        ax.bar(x, relative_auc, yerr=relative_stderr, color=COLORS[learner], tick_label='')
 
 plt.show()
